5c20ff5c9657f7932b5dfe11113426e726d80f75
aecf <81099969+albertoecf@users.noreply.github.com>
2023-02-08 07:03:15 +0100

    feat: connect with unplash api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+const UNPLASH_KEY = process.env.REACT_APP_UNPLASH_KEY;
+
 function App() {
   let [word, setWord] = useState("");
 
   function handleSearchSubmit(e) {
     e.preventDefault();
     console.log(word);
+    let url_to_fetch = `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNPLASH_KEY}`;
+    fetch(url_to_fetch)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
