3b5e4413cd88596d4e62da9770e1affc27678414
aecf <81099969+albertoecf@users.noreply.github.com>
2023-02-11 10:58:46 +0100

    temp: redirect to image

--- api/main.py
+++ api/main.py
@@ -1,31 +1,38 @@
-import requests
-from flask import Flask, request
 import os
-
 from dotenv import load_dotenv
+import requests
+from flask import Flask, request, redirect
 
 UNPLASH_URL = "https://api.unsplash.com/photos/random"
 
 load_dotenv(".env.local")
 UNPLASH_KEY = os.getenv("UNPLASH_KEY")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
+if not UNPLASH_KEY:
+    raise EnvironmentError(
+        "Pleae create .env.local file and insert there UNPLASH_KEY")
 
 app = Flask(__name__)
+
+app.config["DEBUG"] = DEBUG
+
 
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
     headers = {
-        "Accept-Version":"v1",
-        "Authorization" : "Client-ID " + UNPLASH_KEY
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID " + UNPLASH_KEY
     }
     params = {
-        "query" : word
+        "query": word
     }
     response = requests.get(url=UNPLASH_URL, headers=headers, params=params)
     data = response.json()
     download_url = data["links"]["download"]
-    return download_url
+    return redirect(download_url) 
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
