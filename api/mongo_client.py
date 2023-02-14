ea02daab0c1040a38543b2905be0d7079dbecfd1
aecf <81099969+albertoecf@users.noreply.github.com>
2023-02-14 07:54:40 +0100

    feat: add mongo client based on environment

---
+++ api/mongo_client.py
@@ -0,0 +1,11 @@
+import os
+from pymongo import MongoClient
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongo")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD")
+MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
+
+mongo_client = MongoClient(
+    host=MONGO_URL, username=MONGO_USERNAME, password=MONGO_PASSWORD, port=MONGO_PORT
+)
